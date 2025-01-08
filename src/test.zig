121e62115a289dd91272878c7c72934149b7d61f
TheSnak7 <51061669+TheSnak7@users.noreply.github.com>
2025-01-08 07:16:28 +0100

    Add test for the SingleBlockingExecutor

--- src/test.zig
+++ src/test.zig
@@ -2,6 +2,8 @@ const std = @import("std");
 const zasync = @import("zasync");
 const Future = zasync.Future;
 const EternalFuture = zasync.EternalFuture;
+const Executor = zasync.Executor;
+const SingleBlockingExecutor = zasync.SingleBlockingExecutor;
 
 test "EternalFuture" {
     var eternal = EternalFuture.init();
@@ -15,4 +17,46 @@ test "EternalFuture" {
             },
         };
     }
+}
+
+const CountingFuture = struct {
+    counter: u32,
+    max: u32,
+
+    fn poll(ctx: *anyopaque) Future.State!void {
+        var self: *CountingFuture = @alignCast(@ptrCast(ctx));
+
+        if (self.counter < self.max) {
+            self.counter += 1;
+            std.debug.print("\nIncremented counter: {}", .{self.counter});
+            return Future.Pending;
+        } else {
+            return Future.Ready;
+        }
+    }
+
+    pub fn init(max: u32) CountingFuture {
+        return .{
+            .counter = 0,
+            .max = max,
+        };
+    }
+
+    pub fn future(self: *CountingFuture) Future {
+        return .{
+            .ptr = self,
+            .vtable = &.{
+                .poll = poll,
+            },
+        };
+    }
+};
+
+test "CountingFuture" {
+    var counting_future = CountingFuture.init(10);
+    var fut = counting_future.future();
+
+    var sbe = SingleBlockingExecutor.init();
+
+    sbe.blockOn(&fut);
 }
