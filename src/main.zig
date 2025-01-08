c17c18872828093c9df275de2c3b62ed0ce32bba
TheSnak7 <51061669+TheSnak7@users.noreply.github.com>
2025-01-08 06:05:20 +0100

    Split future into own file

--- src/main.zig
+++ src/main.zig
@@ -1,36 +1,4 @@
-pub const Future = struct {
-    ptr: *anyopaque,
-    vtable: *const VTable,
+const future = @import("future.zig");
 
-    pub const Pending = error.Pending;
-    pub const Ready = error.Ready;
-
-    pub const State = error{ Pending, Ready };
-
-    pub const VTable = struct {
-        poll: *const fn (ctx: *anyopaque) State!void,
-    };
-
-    pub fn poll(ctx: *Future) State!void {
-        return try ctx.vtable.*.poll(ctx);
-    }
-};
-
-pub const EternalFuture = struct {
-    fn poll(_: *anyopaque) Future.State!void {
-        return Future.Pending;
-    }
-
-    pub fn init() EternalFuture {
-        return .{};
-    }
-
-    pub fn future(self: *EternalFuture) Future {
-        return .{
-            .ptr = self,
-            .vtable = &.{
-                .poll = poll,
-            },
-        };
-    }
-};
+pub const Future = future.Future;
+pub const EternalFuture = future.Future;
